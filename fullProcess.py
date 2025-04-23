6195bd092ea74ee79e68892c9bc52fefca5893b8
jhenry77 <108282181+jhenry77@users.noreply.github.com>
2025-04-23 00:49:50 -0400

    commented out a command

--- fullProcess.py
+++ fullProcess.py
@@ -156,7 +156,7 @@ if __name__ == "__main__":
     # Combine HTML files
     combined_html = combine_html_files(html_files)
 
-    command = "wkhtmltopdf --enable-javascript --disable-smart-shrinking --no-stop-slow-scripts --debug-javascript --enable-local-file-access --print-media-type --javascript-delay 3000 --user-style-sheet myStyles.css \"combined.html\" documentation.pdf"
+    # command = "wkhtmltopdf --enable-javascript --disable-smart-shrinking --no-stop-slow-scripts --debug-javascript --enable-local-file-access --print-media-type --javascript-delay 3000 --user-style-sheet myStyles.css \"combined.html\" documentation.pdf"
     command = [
     "/usr/local/bin/wkhtmltopdf",  # or just "wkhtmltopdf" if PATH is correct
     "--enable-javascript",
