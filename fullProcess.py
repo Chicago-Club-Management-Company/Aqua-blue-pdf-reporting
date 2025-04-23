a1c26fabe12926e03c0fc3771542645cad2605bf
jhenry77 <108282181+jhenry77@users.noreply.github.com>
2025-04-22 23:26:30 -0400

    fixed the command set up

--- fullProcess.py
+++ fullProcess.py
@@ -156,5 +156,18 @@ if __name__ == "__main__":
     # Combine HTML files
     combined_html = combine_html_files(html_files)
 
-    command = "wkhtmltopdf --enable-javascript --disable-smart-shrinking --no-stop-slow-scripts --debug-javascript --enable-local-file-access --print-media-type --javascript-delay 3000 --user-style-sheet myStyles.css combined.html documentation.pdf"
+    command = [
+    "wkhtmltopdf",
+    "--enable-javascript",
+    "--disable-smart-shrinking",
+    "--no-stop-slow-scripts",
+    "--debug-javascript",
+    "--enable-local-file-access",
+    "--print-media-type",
+    "--javascript-delay", "3000",
+    "--user-style-sheet", "myStyles.css",
+    "combined.html",
+    "documentation.pdf"
+    ]    
+    
     subprocess.run(["/usr/bin/wkhtmltopdf", command])
