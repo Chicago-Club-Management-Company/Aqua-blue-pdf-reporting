cbcd8c10afb30288eb142127a944fc5a6b05d09c
jhenry77 <108282181+jhenry77@users.noreply.github.com>
2025-04-23 00:04:37 -0400

    fixed yml and python script

--- fullProcess.py
+++ fullProcess.py
@@ -158,7 +158,7 @@ if __name__ == "__main__":
 
     command = "wkhtmltopdf --enable-javascript --disable-smart-shrinking --no-stop-slow-scripts --debug-javascript --enable-local-file-access --print-media-type --javascript-delay 3000 --user-style-sheet myStyles.css \"combined.html\" documentation.pdf"
     command = [
-    "/usr/bin/wkhtmltopdf",  # or just "wkhtmltopdf" if PATH is correct
+    "/usr/local/bin/wkhtmltopdf",  # or just "wkhtmltopdf" if PATH is correct
     "--enable-javascript",
     "--disable-smart-shrinking",
     "--no-stop-slow-scripts",
