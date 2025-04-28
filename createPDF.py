a6cefb3bfa8c31753243551b9ec110673c085347
jhenry77 <108282181+jhenry77@users.noreply.github.com>
2025-04-28 13:18:21 -0400

    new version

--- createPDF.py
+++ createPDF.py
@@ -7,7 +7,10 @@ import re
 
 # command = "wkhtmltopdf --enable-javascript --disable-smart-shrinking --no-stop-slow-scripts --debug-javascript --enable-local-file-access --print-media-type --javascript-delay 3000 --user-style-sheet myStyles.css \"combined.html\" documentation.pdf"
 command = [
-"/usr/local/bin/wkhtmltopdf",  # or just "wkhtmltopdf" if PATH is correct
+"wkhtmltopdf",
+# "/usr/local/bin/wkhtmltopdf",
+"--log-level info",
+  # or just "wkhtmltopdf" if PATH is correct
 "--enable-javascript",
 "--debug-javascript",
 "--disable-smart-shrinking",
@@ -19,12 +22,6 @@ command = [
 "combined.html",
 "documentation.pdf"
 ]
-with open("wkhtmltopdf.log", "w") as log_file:
-    try:
-        result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True, check=True)
-        log_file.write(result.stdout + result.stderr)  # Capture all output
-        print("✅ wkhtmltopdf executed successfully!")
-    except subprocess.CalledProcessError as e:
-        log_file.write(f"ERROR: Subprocess failed with exit code {e.returncode}\n")
-        log_file.write(e.stdout + e.stderr)
-        print(f"❌ Error! Check wkhtmltopdf.log for details.")
+
+
+subprocess.run(command)
