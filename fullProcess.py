986c3d4f444d464364f0d261120130a41038d198
jhenry77 <108282181+jhenry77@users.noreply.github.com>
2025-04-28 11:51:39 -0400

    fixed command

--- fullProcess.py
+++ fullProcess.py
@@ -151,20 +151,20 @@ if __name__ == "__main__":
     # Combine HTML files
     combined_html = combine_html_files(html_files)
 
-    command = "wkhtmltopdf --enable-javascript --disable-smart-shrinking --no-stop-slow-scripts --debug-javascript --enable-local-file-access --print-media-type --javascript-delay 3000 --user-style-sheet myStyles.css \"combined.html\" documentation.pdf"
-    # command = [
-    # "xvfb-run",
-    # "/usr/local/bin/wkhtmltopdf",  # or just "wkhtmltopdf" if PATH is correct
-    # "--enable-javascript",
-    # "--disable-smart-shrinking",
-    # "--no-stop-slow-scripts",
-    # "--enable-local-file-access",
-    # "--print-media-type",
-    # "--javascript-delay", "3000",
-    # "--user-style-sheet", "myStyles.css",
-    # "combined.html",
-    # "documentation.pdf"
-    # ]
+    # command = "wkhtmltopdf --enable-javascript --disable-smart-shrinking --no-stop-slow-scripts --debug-javascript --enable-local-file-access --print-media-type --javascript-delay 3000 --user-style-sheet myStyles.css \"combined.html\" documentation.pdf"
+    command = [
+    "xvfb-run",
+    "/usr/local/bin/wkhtmltopdf",  # or just "wkhtmltopdf" if PATH is correct
+    "--enable-javascript",
+    "--disable-smart-shrinking",
+    "--no-stop-slow-scripts",
+    "--enable-local-file-access",
+    "--print-media-type",
+    "--javascript-delay", "3000",
+    "--user-style-sheet", "myStyles.css",
+    "combined.html",
+    "documentation.pdf"
+    ]
     try:
         result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=True)
         print(result.stdout.decode())
