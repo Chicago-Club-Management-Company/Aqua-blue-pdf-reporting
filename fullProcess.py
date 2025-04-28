9378c81ceec25c4815b519aecbf347ade9c95163
jhenry77 <108282181+jhenry77@users.noreply.github.com>
2025-04-28 11:45:47 -0400

    updated again

--- fullProcess.py
+++ fullProcess.py
@@ -153,11 +153,11 @@ if __name__ == "__main__":
 
     # command = "wkhtmltopdf --enable-javascript --disable-smart-shrinking --no-stop-slow-scripts --debug-javascript --enable-local-file-access --print-media-type --javascript-delay 3000 --user-style-sheet myStyles.css \"combined.html\" documentation.pdf"
     command = [
+    "xvfb-run",
     "/usr/local/bin/wkhtmltopdf",  # or just "wkhtmltopdf" if PATH is correct
     "--enable-javascript",
     "--disable-smart-shrinking",
     "--no-stop-slow-scripts",
-    "--debug-javascript",
     "--enable-local-file-access",
     "--print-media-type",
     "--javascript-delay", "3000",
