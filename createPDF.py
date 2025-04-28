906cd00812809e4cd27a80716c800b6d9196c041
jhenry77 <108282181+jhenry77@users.noreply.github.com>
2025-04-28 13:25:26 -0400

    trying a different format

--- createPDF.py
+++ createPDF.py
@@ -5,23 +5,22 @@ import subprocess
 import re
 
 
-# command = "wkhtmltopdf --enable-javascript --disable-smart-shrinking --no-stop-slow-scripts --debug-javascript --enable-local-file-access --print-media-type --javascript-delay 3000 --user-style-sheet myStyles.css \"combined.html\" documentation.pdf"
-command = [
-"wkhtmltopdf",
-# "/usr/local/bin/wkhtmltopdf",
-"--log-level",
-  # or just "wkhtmltopdf" if PATH is correct
-"--enable-javascript",
-"--debug-javascript",
-"--disable-smart-shrinking",
-"--no-stop-slow-scripts",
-"--enable-local-file-access",
-"--print-media-type",
-"--javascript-delay", "3000",
-"--user-style-sheet", "myStyles.css",
-"combined.html",
-"documentation.pdf"
-]
+command = "/usr/local/bin/wkhtmltopdf --log-level info --enable-javascript --disable-smart-shrinking --no-stop-slow-scripts --debug-javascript --enable-local-file-access --print-media-type --javascript-delay 3000 --user-style-sheet myStyles.css \"combined.html\" documentation.pdf"
+# command = [
+# "wkhtmltopdf",
+# # "/usr/local/bin/wkhtmltopdf",
+# "--log-level info", # or just "wkhtmltopdf" if PATH is correct
+# "--enable-javascript",
+# "--debug-javascript",
+# "--disable-smart-shrinking",
+# "--no-stop-slow-scripts",
+# "--enable-local-file-access",
+# "--print-media-type",
+# "--javascript-delay", "3000",
+# "--user-style-sheet", "myStyles.css",
+# "combined.html",
+# "documentation.pdf"
+# ]
 
 
 subprocess.run(command)
