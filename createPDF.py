6a9a3247ca93c9a89f24553502e5c70951334ae2
jhenry77 <108282181+jhenry77@users.noreply.github.com>
2025-04-28 13:38:55 -0400

    fixed issue

--- createPDF.py
+++ createPDF.py
@@ -5,7 +5,7 @@ import subprocess
 import re
 
 
-command = "/usr/local/bin/wkhtmltopdf --log-level info --enable-javascript --disable-smart-shrinking --no-stop-slow-scripts --debug-javascript --enable-local-file-access --print-media-type --javascript-delay 3000 --user-style-sheet myStyles.css \"combined.html\" documentation.pdf"
+command = "/usr/local/bin/wkhtmltopdf --enable-javascript --disable-smart-shrinking --no-stop-slow-scripts --debug-javascript --enable-local-file-access --print-media-type --javascript-delay 3000 --user-style-sheet myStyles.css \"combined.html\" documentation.pdf"
 # command = [
 # "wkhtmltopdf",
 # # "/usr/local/bin/wkhtmltopdf",
