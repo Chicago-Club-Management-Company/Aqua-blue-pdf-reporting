2d7e1010decf0db27450b65ab73485089997cd40
jhenry77 <108282181+jhenry77@users.noreply.github.com>
2025-04-28 11:14:47 -0400

    added a check, this may fix the issue

--- fullProcess.py
+++ fullProcess.py
@@ -165,7 +165,7 @@ if __name__ == "__main__":
     "combined.html",
     "documentation.pdf"
     ]
-    subprocess.run(command)
+    subprocess.run(command, check = True)
 
     print("job is completed and pdf has been created")
 
